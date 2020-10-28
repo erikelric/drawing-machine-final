2f061d75873128145d5403a6ac9108ebe5840e1e
erikelric <60452164+erikelric@users.noreply.github.com>
2020-10-27 19:17:48 -0700

    update

--- sketch.js
+++ sketch.js
@@ -1,22 +1,41 @@
 let array = [];
+let strokeWidth = 5;
+let colorPicker;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(220, 50, 133);
+  background(220);
 
   strokeWeight(5);
 
+    // colorPicker = createColorPicker('#ed225d');
+    // colorPicker.position(0, height + 5);
 
 }
 
+
 function draw() {
+
+  // background(colorPicker.color());
+  background(220, 5);
+  strokeWeight(strokeWidth);
 
 if (mouseIsPressed){
   // line(mouseX, mouseY, pmouseX, pmouseY);
-  stroke(map(mouseX, 0, 600, 0, 255, true))
+  // stroke(map(mouseX, 0, 600, 0, 255, true))
+
+  //Symmetric drawing
+  // ellipse(mouseX, mouseY, 4, 4);
+
+  // ellipse(mouseX, height - mouseY, 4, 4);
+  // ellipse(width - mouseX, mouseY, 4, 4);
+  // ellipse(width - mouseX, height - mouseY, 4, 4);
+
   // array.push([mouseX, mouseY]);
-  line(width - mouseX, height- mouseY, width - pmouseX, height - pmouseY);
+
   line(mouseX, mouseY, pmouseX, pmouseY);
+  line(width - mouseX, mouseY, width - pmouseX, pmouseY);
+
   }
 
 }
@@ -26,9 +45,10 @@ function keyTyped(){
     if (key === `s`){
       //save this image
       saveCanvas(`fileName`, `png`);
-    } else if (key === 'd'){
-      // diplay the image
+    } else if (key === 'c'){
+      // clear the image
       background(255);
+      clear();
 
       for(let i = 0; i < array.length; i++){
         // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
